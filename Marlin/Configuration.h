0a2d33f9974f11af2562e6a980578aea432014e6
남훈 장 <nam@lincsolution.com>
2023-07-20 15:02:20 +0900

    23.07.20

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1751,7 +1751,7 @@
 #define Z_SAFE_HOMING //LINCSOLUTION
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT -23.0  // X point for Z homing(24, 254)
+  #define Z_SAFE_HOMING_X_POINT -26.0  // X point for Z homing(24, 254)
   #define Z_SAFE_HOMING_Y_POINT 260.0 // Y point for Z homing
 #endif
 
