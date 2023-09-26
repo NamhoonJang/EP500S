a867f75784656f9bd48901550152493e150b1952
남훈 장 <nam@lincsolution.com>
2023-09-26 15:48:29 +0900

    z_homing speed changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1756,7 +1756,7 @@
 #endif
 
 // Homing speeds (mm/min)
-#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (10*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (2*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
