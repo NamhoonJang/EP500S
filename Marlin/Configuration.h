ea3bb5b1a221ecf992199f4e4a173e75cbe4e936
남훈 장 <nam@lincsolution.com>
2024-01-16 10:37:07 +0900

    default bed pid kp/ki/kd changed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -651,9 +651,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 13.20
-  #define DEFAULT_bedKi  0.27
-  #define DEFAULT_bedKd 300.96
+  #define DEFAULT_bedKp  16.0
+  #define DEFAULT_bedKi   0.1
+  #define DEFAULT_bedKd 300.0
 //kp: 11.84 / ki:0.26 / kd:352.96
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
