1397c1cd24790a5fd192758debf20d3b83a835a0
wlstj3507 <wlstj3507@lincsolution.com>
2024-11-26 18:34:41 +0900

    POSITION CHANGE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1751,7 +1751,7 @@
 #define Z_SAFE_HOMING //LINCSOLUTION
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT -13.0  // X point for Z homing(-20, 262)
+  #define Z_SAFE_HOMING_X_POINT -18.0  // X point for Z homing(-20, 262)
   #define Z_SAFE_HOMING_Y_POINT 264.0 // Y point for Z homing
 #endif
 
