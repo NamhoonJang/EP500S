a682282959ab8af0fe45b778c122860a2b967a54
wlstj3507 <wlstj3507@lincsolution.com>
2024-11-26 18:37:36 +0900

    동박위치 원복

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1751,8 +1751,8 @@
 #define Z_SAFE_HOMING //LINCSOLUTION
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT -18.0  // X point for Z homing(-20, 262)
-  #define Z_SAFE_HOMING_Y_POINT 264.0 // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT -20.0  // X point for Z homing(-20, 262)
+  #define Z_SAFE_HOMING_Y_POINT 262.0 // Y point for Z homing
 #endif
 
 // Homing speeds (mm/min)
